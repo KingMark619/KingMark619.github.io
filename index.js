de4aa3a34b6046b2605a13d98c4b42f3f1db3163
Marco Kayumba <marcokayumba@icloud.com>
2020-06-15 11:31:55 +0800

    updateLogos

---
+++ index.js
@@ -0,0 +1,13 @@
+const hey = " hey there";
+console.log("loaded");
+function checkTest() {
+  alert("Image is loaded");
+}
+const content = [
+  {
+    name: "",
+    tag: "",
+    link: "",
+    image: "",
+  },
+];
